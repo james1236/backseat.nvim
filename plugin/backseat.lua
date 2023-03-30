9a673a01414529045e66932e425569cc2090e5e6
james1236 <32351696+james1236@users.noreply.github.com>
2023-03-30 19:07:14 +0100

    Add error handling for API requests

--- plugin/backseat.lua
+++ plugin/backseat.lua
@@ -11,67 +11,89 @@ require("backseat").setup()
 
 -- local http = require("http")
 
-local request_data = {
+local requestData = {
     model = "gpt-3.5-turbo",
     messages = { {
         role = "user",
-        content = "Hello!"
+        content = "What are some code readability tips?"
     } },
 }
 
-local function gpt_request(data, api_key)
+local function print(msg)
+    _G.print("Backseat > " .. msg)
+end
+
+local function gpt_request(dataJSON, api_key)
+    if api_key == nil then
+        print("No API key found. Please set g:openai_api_key")
+        return nil
+    end
+
     local curlRequest = string.format(
         "curl -s https://api.openai.com/v1/chat/completions -H \"Content-Type: application/json\" -H \"Authorization: Bearer " ..
-        api_key .. "\" -d '" .. data .. "'"
+        api_key .. "\" -d '" .. dataJSON .. "'"
     )
-    print(curlRequest)
+    -- print(curlRequest)
 
     local response = vim.fn.system(curlRequest)
-    local responseTable = vim.fn.json_decode(response)
+    local success, responseTable = pcall(vim.json.decode, response)
 
-    print(response)
+    if success == false or responseTable == nil then
+        print("Bad or no response: " .. response)
+        return nil
+    end
+
+    if responseTable.error ~= nil then
+        print("OpenAI Error: " .. responseTable.error.message)
+        return nil
+    end
+
+    -- print(response)
     return responseTable
 end
 
 local function parseResponse(response)
-
+    print("AI Says: " .. response.choices[1].message.content)
 end
 
-vim.api.nvim_create_user_command("BackseatAuthKey", function(opt)
-    -- local bufnr = tonumber(opt.args)
-    -- require("ccc.highlighter"):enable(bufnr)
-end, { nargs = "?" })
+-- vim.api.nvim_create_user_command("BackseatAuthKey", function(opt)
+--     -- local bufnr = tonumber(opt.args)
+--     -- require("ccc.highlighter"):enable(bufnr)
+-- end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("Backseat", function()
-    print("Backseat setup: " .. vim.inspect(vim.g.loaded_backseat))
+    -- print("Backseat setup: " .. vim.inspect(vim.g.loaded_backseat))
 
-    local request = vim.fn.json_encode(request_data)
-    -- local response = gpt_request(request, vim.g.openai_api_key)
+    local requestJSON = vim.json.encode(requestData)
+    local responseTable = gpt_request(requestJSON, vim.g.openai_api_key)
+    if responseTable == nil then
+        return nil
+    end
 
-    local response = vim.fn.json_decode([[
-    {
-      "id": "chatcmpl-<id>",
-      "object": "chat.completion",
-      "created": 1680192412,
-      "model": "gpt-3.5-turbo-0301",
-      "usage": {
-        "prompt_tokens": 10,
-        "completion_tokens": 9,
-        "total_tokens": 19
-      },
-      "choices": [
-        {
-          "message": {
-            "role": "assistant",
-            "content": "Hello! How can I assist you today?"
-          },
-          "finish_reason": "stop",
-          "index": 0
-        }
-      ]
-    }]])
+    -- local response = vim.fn.json_decode([[
+    -- {
+    --   "id": "chatcmpl-<id>",
+    --   "object": "chat.completion",
+    --   "created": 1680192412,
+    --   "model": "gpt-3.5-turbo-0301",
+    --   "usage": {
+    --     "prompt_tokens": 10,
+    --     "completion_tokens": 9,
+    --     "total_tokens": 19
+    --   },
+    --   "choices": [
+    --     {
+    --       "message": {
+    --         "role": "assistant",
+    --         "content": "Hello! How can I assist you today?"
+    --       },
+    --       "finish_reason": "stop",
+    --       "index": 0
+    --     }
+    --   ]
+    -- }]])
 
-    parseResponse(response)
+    parseResponse(responseTable)
 
     -- require("backseat.main"):run()
 end, {})
